f08226355d909f895aa8ce13984049cab8f3541f
Ruby Quail <rupertmoore@ruby-imac.local>
2019-12-30 12:46:35 +1100

    setup grid and canvas sizing
    Signed-off-by: Ruby Quail <rupertmoore@ruby-imac.local>

--- src/components/MainApp.tsx
+++ src/components/MainApp.tsx
@@ -1,16 +1,39 @@
-import React, { ReactElement, CSSProperties } from 'react';
+import React, {
+  ReactElement,
+  CSSProperties,
+  useState,
+  useEffect,
+} from 'react';
+import SideBar from './SideBar';
+import DrawingCanvas from './DrawingCanvas';
 
 interface Props {}
-
 function MainApp({}: Props): ReactElement {
+  const saveFunction = (dataUrl: string) => {};
+  const handleLoad = () => {};
+  let imageSize = 512;
+  let [selectedDrawing, selectDrawing] = useState(
+    'Drawing 1'
+  );
+  const maximumCanvasSize = Math.min(
+    window.innerHeight,
+    2 * (window.innerWidth / 3)
+  );
+  imageSize = 52 * Math.floor(maximumCanvasSize / 64);
   const appStyling: CSSProperties = {
     display: 'grid',
     gridTemplateColumns: '1fr 2fr',
+    gridTemplateAreas: 'sidebar canvas',
   };
+
   return (
-    <div style={appStyling}>
-      <h1>App</h1>
-    </div>
+    <>
+      <h1>App {imageSize}</h1>
+      <div style={{ ...appStyling }}>
+        <SideBar />
+        <DrawingCanvas size={imageSize} />
+      </div>
+    </>
   );
 }
 
